f39cbe584f34636a8cf412883852295bd6479d82
treetzeng <treetzeng@lctech.com.tw>
2021-06-16 22:25:18 +0800

    write all comments once

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ const headers = {
 };
 
 const getCommentUrl = (countStart = 0) => {
-	return `https://www.google.com.tw/maps/preview/review/listentitiesreviews?authuser=0&hl=zh-TW&gl=tw&pb=!1m2!1y3765758551715385081!2y17436170062305043679!2m2!1i${countStart}!2i10!3e2!4m5!3b1!4b1!5b1!6b1!7b1!5m2!1sTf7JYOquJpji-AbVspzwDQ!7e81`
+	return `https://www.google.com.tw/maps/preview/review/listentitiesreviews?authuser=0&hl=zh-TW&gl=tw&pb=!1m2!1y3765758551715385081!2y17436170062305043679!2m2!1i${countStart}!2i10!3e2!4m5!3b1!4b1!5b1!6b1!7b1!5m2!1sTf7JYOquJpji-AbVspzwDQ!7e81`;
 };
 
 /** @type {axios.AxiosRequestConfig} */
@@ -29,36 +29,45 @@ const axiosConfig = {
  * @param {{fileName: string; url: string;}} options
  */
 const apiGetTaipeitMcdonaldComments = async (options) => {
-	await axios({ ...axiosConfig, url: options.url }).then((res) => {
-		const resData = res.data.replace(")]}'", '');
-		const dataObj = JSON.parse(resData);
-		const commentDataList = dataObj[2];
+	const res = await axios({ ...axiosConfig, url: options.url });
 
-		const commentList = commentDataList.map((commentData) =>
-			formatCommentData(commentData)
-		);
+	const resData = res.data.replace(")]}'", '');
+	const dataObj = JSON.parse(resData);
+	const commentDataList = dataObj[2];
 
-		writeJsonFile({
-			fileName: options.fileName,
-			data: commentList,
-		});
-	});
+	const commentList = commentDataList.map((commentData) =>
+		formatCommentData(commentData)
+	);
+
+	return commentList;
 };
 
 const loopWithDelay = async () => {
 	createFileDirectory();
+	const allComents = [];
 
 	const fileName = 'taipei_mcdonald';
 	let count = 0;
 	for (let i = 0; i < 10; i++) {
-		await apiGetTaipeitMcdonaldComments({
+		const commentList = await apiGetTaipeitMcdonaldComments({
 			fileName: fileName + (i + 1),
 			url: getCommentUrl(count),
 		});
 		count += 10;
 
+		allComents.push(commentList);
+
+		writeJsonFile({
+			fileName: fileName + (i + 1),
+			data: commentList,
+		});
 		await delay(2123 + Math.floor(Math.random() * 200));
 	}
+
+	writeJsonFile({
+		fileName: 'allComments',
+		data: allComents,
+	});
 };
 
 loopWithDelay();
